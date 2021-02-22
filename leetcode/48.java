eb859e7cc737d72aa089108ce9807198c53a8fe4
Minyong Jeong <jmy3155@gmail.com>
2021-02-22 22:19:12 +0900

    48. Rotate Image (Medium)

---
+++ leetcode/48.java
@@ -0,0 +1,27 @@
+/*
+    https://leetcode.com/problems/rotate-image/
+    48. Rotate Image (Medium)
+*/
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        
+        // transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+        
+        // reverse
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-1-j];
+                matrix[i][n-1-j] = tmp;
+            }
+        }
+    }
+}
