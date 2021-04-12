af2d284388141fbd4c9cd09d514326dee71adc60
Minyong Jeong <jmy3155@gmail.com>
2021-04-13 00:40:49 +0900

    Update 743.java

--- leetcode/743.java
+++ leetcode/743.java
@@ -5,46 +5,48 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         Map<Integer, List<int[]>> graph = new HashMap();
-        for (int[] edge : times) {
-            if (!graph.containsKey(edge[0])) {
-                graph.put(edge[0], new ArrayList<int[]>());
+        for (int[] info : times) {
+            // info[0]: sourceNode, info[1]: targetNode, info[2]: time
+            if (!graph.containsKey(info[0])) {
+                graph.put(info[0], new ArrayList<int[]>());
             }
-            graph.get(edge[0]).add(new int[]{edge[1], edge[2]});
+            graph.get(info[0]).add(new int[]{info[1], info[2]});
         }
-        
+
         Map<Integer, Integer> dist = new HashMap();
         for (int i = 1; i <= n; i++) {
             dist.put(i, Integer.MAX_VALUE);
         }
         dist.put(k, 0);
-        
+
         boolean[] visited = new boolean[n+1];
-        
+
         while (true) {
-            int candNode = -1;
-            int candDist = Integer.MAX_VALUE;
+            int node = -1;
+            int nodeDist = Integer.MAX_VALUE;
             for (int i = 1; i <= n; i++) {
-                if (!visited[i] && dist.get(i) < candDist) {
-                    candDist = dist.get(i);
-                    candNode = i;
+                if (!visited[i] && dist.get(i) < nodeDist) {
+                    nodeDist = dist.get(i);
+                    node = i;
                 }
             }
-            
-            if (candNode < 0) break;
-            
-            visited[candNode] = true;
-            if (graph.containsKey(candNode)) {
-                for (int[] info : graph.get(candNode)) {
-                    dist.put(info[0], Math.min(dist.get(info[0]), dist.get(candNode) + info[1]));
+
+            if (node < 0) break;
+
+            visited[node] = true;
+            if (graph.containsKey(node)) {
+                for (int[] info : graph.get(node)) {
+                    dist.put(info[0], Math.min(dist.get(info[0]), dist.get(node) + info[1]));
                 }
             }
         }
-        
+
         int ans = 0;
-        for (int cand : dist.values()) {
-            if (cand == Integer.MAX_VALUE) return -1;
-            ans = Math.max(ans, cand);
-            System.out.print(cand + " ");
+        for (int d : dist.values()) {
+            if (d == Integer.MAX_VALUE) {
+                return -1;
+            }
+            ans = Math.max(ans, d);
         }
 
         return ans;
